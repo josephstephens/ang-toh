33581542a1cc9e7e96a13b148f2baf299371bf40
josephstephens <jstephens@fusionalliance.com>
2018-08-01 18:01:30 -0400

    eliminate calls to unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { Test1Component } from './test1/test1.component';
-import { HeroesTest1Component } from './heroes-test1/heroes-test1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    Test1Component,
-    HeroesTest1Component
+    HeroesComponent
   ],
   imports: [
     BrowserModule
